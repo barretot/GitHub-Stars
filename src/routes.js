111666322396d06d6addbab458284c35f943fbe1
barretot <barretoruan77@gmail.com>
2021-05-09 06:32:54 -0300

    fix

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import RepositoryController from './database/app/controllers/RepositoryControlle
 const routes = Router();
 
 routes.post('/api/repositorie', RepositoryController.store);
-routes.get('/api/tags', RepositoryController.storeTags);
+routes.get('/api/repositories/tags', RepositoryController.storeTags);
 
 routes.get('/api/repositories', RepositoryController.index);
 routes.delete('/api/repositorie/:id', RepositoryController.destroy);
