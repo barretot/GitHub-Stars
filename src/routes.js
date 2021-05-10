7934320268b364bb772a7eb4ba66d89662050f72
barretot <barretoruan77@gmail.com>
2021-05-10 03:31:06 -0300

    Repositorios com estrelas criado

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const routes = Router();
 routes.post('/api/repositorie', RepositoryController.store);
 routes.get('/api/repositories/tags', RepositoryController.storeTags);
 
+routes.get('/api/repositories/stars', RepositoryController.getRepoStars);
+
 routes.get('/api/repositories', RepositoryController.index);
 routes.delete('/api/repositorie/:id', RepositoryController.destroy);
 
